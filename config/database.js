0bae3a1a4a49868e2ded5774c1bb982db0098050
brupadiro <brunodiharce@gmail.com>
2023-12-01 08:30:10 -0300

    fdfd

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: 'dpg-clkrmncjtl8s73f17epg-a.oregon-postgres.render.com/batumidb',
+      host: 'dpg-clkrmncjtl8s73f17epg-a',
       port: '5432',
       database: 'batumidb',
       user: 'batumiuser',
