d7da7a84fe6980c7ee4d5cfbf5a6f170ab9fe02f
brupadiro <brunodiharce@gmail.com>
2023-12-01 08:16:43 -0300

    ddd

--- config/database.js
+++ config/database.js
@@ -5,14 +5,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: 'dpg-clkrmncjtl8s73f17epg-a.oregon-postgres.render.com/batumidb',
+      host: 'dpg-clkrmncjtl8s73f17epg-a',
       port: '5432',
       database: 'batumidb',
       user: 'batumiuser',
       password: 'E0rXLly5eVDZO2YEhuIImrnljhgrLziA',
-      ssl: {
-        ca: fs.readFileSync(`./config/ca-certificate.crt`).toString(),
-      },
+      ssl: true // Habilitar SSL/TLS
 },
     useNullAsDefault: true,
   },
