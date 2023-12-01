da7334e5ee54ed0c8098f035df37c43b22549dc4
brupadiro <brunodiharce@gmail.com>
2023-12-01 08:23:51 -0300

    ssl

--- config/database.js
+++ config/database.js
@@ -5,13 +5,15 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: 'dpg-clkrmncjtl8s73f17epg-a',
+      host: 'dpg-clkrmncjtl8s73f17epg-a.oregon-postgres.render.com/batumidb',
       port: '5432',
       database: 'batumidb',
       user: 'batumiuser',
       password: 'E0rXLly5eVDZO2YEhuIImrnljhgrLziA',
-      ssl: true // Habilitar SSL/TLS
-},
+      ssl: {
+        rejectUnauthorized: false // Deshabilitar la verificación del certificado
+      },
+  },
     useNullAsDefault: true,
   },
 });
